878c731cc1fa75b7b7ce1622d675c7fc9e1447fa
Ahlan Guarnier <ahlan90@gmail.com>
2020-04-09 17:43:37 -0300

    Nova tela de login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'http://institutohabilitar-dev.herokuapp.com',
+  api: 'https://institutohabilitar-dev.herokuapp.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'http://institutohabilitar-dev.herokuapp.com',
+  api: 'https://institutohabilitar-dev.herokuapp.com',
 };
 
 /*
