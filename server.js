e35b677daa1e83d79de1899ad408ef71676bcead
Ahlan Guarnier <ahlan90@gmail.com>
2020-04-09 16:18:15 -0300

    Add port log

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-app.listen(process.env.PORT || 8080, function() {
+var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
