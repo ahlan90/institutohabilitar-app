285e4f086993b1f8e1416659db5b824f8ee83dc9
Ahlan Guarnier <ahlan90@gmail.com>
2020-04-20 14:59:22 -0300

    Acertando o layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from './auth/services/auth.service';
 import { TokenService } from './auth/services/token.service';
 import { of } from 'rxjs';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,22 @@ export class AppComponent implements OnInit, OnDestroy {
   token$;
   token;
 
+  mobileQuery: MediaQueryList;
+
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
   constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
     private authService: AuthService,
     private tokenService: TokenService
-  ) {}
-
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit(): void {
     this.token$ = this.tokenService.getTokenAsObservable();
@@ -28,6 +40,7 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
     this.token$.unsubscribe();
   }
 
